98e2ed5047432ccbdd85ba3022dfdeaa6dc7c7ac
Pratiksha Singh <pratikshasingh923@gmail.com>
2025-02-04 00:12:35 +0530

    updating workout page for local storage

--- src/WorkoutList.js
+++ src/WorkoutList.js
@@ -1,4 +1,3 @@
-// src/WorkoutList.js
 
 import React from 'react';
 
@@ -16,46 +15,41 @@ function WorkoutList({
   
   return (
     <div className="workout-list">
-      
       <select value={filterType} onChange={(e) => setFilterType(e.target.value)}>
-        <option value=""disabled hidden>Filter by Workout Type</option>
+        <option value="" disabled hidden>Filter by Workout Type</option>
         <option value="Cardio">Cardio</option>
         <option value="Strength Training">Strength Training</option>
         <option value="Yoga">Yoga</option>
-        {/* Add more options as needed */}
       </select>
 
-
-      
       {workouts.length > 0 ? (
-  <table className="workout-table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Workouts</th>
-        <th>Number of Workouts</th>
-        <th>Total Workout Minutes</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      {workouts.map((workout, index) => (
-        <tr key={index}>
-          <td>{workout.userName}</td>
-          <td>{workout.workoutType}</td>
-          <td>{workout.workoutType.split(',').length}</td>
-          <td>{workout.workoutMinutes}</td>
-          <td>
-            <button onClick={() => deleteWorkout(index)}>Delete</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-) : (
-  <p>No workouts found.</p>
-)}
-
+        <table className="workout-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Workouts</th>
+              <th>Number of Workouts</th>
+              <th>Total Workout Minutes</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {workouts.map((workout, index) => (
+              <tr key={index}>
+                <td>{workout.userName}</td>
+                <td>{workout.workoutTypes.join(', ')}</td>
+                <td>{workout.workoutCount}</td>
+                <td>{workout.totalMinutes}</td>
+                <td>
+                  <button onClick={() => deleteWorkout(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No workouts found.</p>
+      )}
 
       <div className="pagination">
         <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
